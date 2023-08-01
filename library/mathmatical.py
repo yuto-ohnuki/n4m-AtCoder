56dbd5633ffb7edc637392f8fc5e57555c50d6a3
yuto_ohnuki <yuto.personal92@gmail.com>
2023-08-01 07:12:25 +0100

    add math libs

--- library/mathmatical.py
+++ library/mathmatical.py
@@ -1,3 +1,14 @@
+""" 最大公約数 """
+def gcd(a,b):
+    import math
+    return math.gcd(a,b)
+
+""" 最小公倍数 """
+def lcm(a,b):
+    import math
+    x = math.gcd(a,b)
+    return a*b//x
+
 """ 約数の列挙 """
 def divisor(x):
     ret = []
@@ -7,5 +18,20 @@ def divisor(x):
             if not i**2 == x:
                 ret.append(x//i)
 
+    # ret.sort()
+    return ret
+
+""" 素因数分解 """
+def prime_decomposition(x):
+    ret = []
+    i = 2
+    while i**2 <= x:
+        while x%i == 0:
+            ret.append(i)
+            x = x // i
+        i += 1
+    if x > 1:
+        ret.append(x)
+    
     # ret.sort()
     return ret
