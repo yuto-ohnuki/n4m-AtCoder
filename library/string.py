726887e7afcc880bc38b7645cdf1659fed311edc
yuto_ohnuki <yuto.personal92@gmail.com>
2023-08-06 09:14:05 +0100

    add string.py

---
+++ library/string.py
@@ -0,0 +1,11 @@
+""" 数値 (str) を右寄せゼロ埋め """
+def zero_padding(x, digit):
+    if isinstance(x, int):
+        x = str(x)
+    elif isinstance(x, str):
+        pass
+    else:
+        raise Exception('Unexpected data type: {}'.format(x))
+    return x.zfill(digit)
+
+print(zero_padding(4, 4))
