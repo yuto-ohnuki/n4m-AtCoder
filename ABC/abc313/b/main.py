846d596b18e0dcd5977e64e200f1b2dab48317d0
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-21 14:16:53 +0900

    update abc313

--- ABC/abc313/b/main.py
+++ ABC/abc313/b/main.py
@@ -1,5 +1,25 @@
+from collections import deque
 def main():
-    pass
+    n,m = map(int, input().split())
+    g = [[] for _ in range(n+1)]
+    ret = [set() for _ in range(n+1)]
+    for _ in range(m):
+        a,b =  map(int, input().split())
+        g[a].append(b)
+    for node in range(1, n+1):
+        que = deque(g[node])
+        while que:
+            tmp = que.pop()
+            if tmp not in ret[node]:
+                ret[node].add(tmp)
+                for nx in g[tmp]:
+                    que.append(nx)
+    ans = -1
+    for node in range(1, n+1):
+        tmp = list(ret[node])
+        if len(tmp) == n-1:
+            ans = node
+    print(ans)
 
 if __name__ == '__main__':
     main()
--- ABC/abc313/c/main.py
+++ ABC/abc313/c/main.py
@@ -1,5 +1,15 @@
 def main():
-    pass
+    n = int(input())
+    a = list(map(int, input().split()))
+    src = list(reversed(sorted(a)))
+    q, mod = divmod(sum(a), n)
+    ans = 0
+    for i in range(n):
+        if i<mod:
+            ans += abs(src[i] - (q+1))
+        else:
+            ans += abs(src[i] - q)
+    print(ans//2)
 
 if __name__ == '__main__':
     main()
