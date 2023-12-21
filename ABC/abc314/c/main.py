129fe7e1a92e59e192db5ab354522857c44e13f4
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-21 14:47:22 +0900

    update abc314 a-c

--- ABC/abc314/c/main.py
+++ ABC/abc314/c/main.py
@@ -1,5 +1,19 @@
+from collections import defaultdict, deque
 def main():
-    pass
+    n,m = map(int, input().split())
+    s = input()
+    c = list(map(int, input().split()))
+
+    dic = defaultdict(list)
+    for i in range(n):
+        dic[c[i]].append(s[i])
+    
+    cnt = {key:-1 for key in range(1, m+1)}
+    ans = ""
+    for i in range(n):
+        ans += dic[c[i]][cnt[c[i]]]
+        cnt[c[i]] += 1
+    print(ans)
 
 if __name__ == '__main__':
     main()
