9ec83b8faa0f6e2cc1d3d854a2b8236250a11d92
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-21 16:52:33 +0900

    upload abc314 a-c

--- ABC/abc314/b/main.py
+++ ABC/abc314/b/main.py
@@ -1,5 +1,26 @@
 def main():
-    pass
+    n = int(input())
+    dic = {}
+    for i in range(n):
+        c = int(input())
+        a = list(map(int, input().split()))
+        dic[i] = a
+    x = int(input())
+    
+    ret = []
+    mn = float('inf')
+    for key, value in dic.items():
+        if x in value:
+            ret.append((key, len(value)))
+            mn = min(mn, len(value))
+    
+    ans = []
+    for i,j in ret:
+        if j == mn:
+            ans.append(i+1)
+    
+    print(len(ans))
+    print(*ans)
 
 if __name__ == '__main__':
     main()
