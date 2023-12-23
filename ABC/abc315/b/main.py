1ccfcf5b1ee582ac8a9915787b6768f406714291
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-23 16:48:21 +0900

    abc315/b

---
+++ ABC/abc315/b/main.py
@@ -0,0 +1,16 @@
+def main():
+    m = int(input())
+    lst = list(map(int, input().split()))
+    mid = (sum(lst)+1)//2
+    ans = [-1, 0]
+    for i in range(m):
+        if ans[1]+lst[i] >= mid:
+            ans = [i+1, mid-ans[1]]
+            break
+        else:
+            ans[1] += lst[i]
+    print(*ans)
+
+
+if __name__ == '__main__':
+    main()
