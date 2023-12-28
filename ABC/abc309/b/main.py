11b1bec474a8a25e97e4bcbc417c017bec363e01
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-28 12:06:13 +0900

    add abac309 b

--- ABC/abc309/b/main.py
+++ ABC/abc309/b/main.py
@@ -1,5 +1,24 @@
 def main():
-    pass
+    n = int(input())
+    mat = [list(input()) for _ in range(n)]
+
+    ans = [[None]*n for _ in range(n)]
+    ans[0][0] = mat[1][0]
+    for i in range(n-1):
+        ans[0][i+1] = mat[0][i] 
+    for i in range(n-1):
+        ans[i+1][-1] = mat[i][-1]
+    for i in reversed(range(n-1)):
+        ans[-1][i] = mat[-1][i+1]
+    for i in reversed(range(n-1)):
+        ans[i][0] = mat[i+1][0]
+
+    for i in range(n):
+        for j in range(n):
+            if ans[i][j] == None:
+                ans[i][j] = mat[i][j]
+    for line in ans:
+        print(''.join(line))
 
 if __name__ == '__main__':
     main()
