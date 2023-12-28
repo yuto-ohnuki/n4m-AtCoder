b353bc3ce5cae8afb3a2cfa7571fa8a43af5b9b6
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-28 11:48:59 +0900

    add abc309 a

--- ABC/abc309/a/main.py
+++ ABC/abc309/a/main.py
@@ -1,5 +1,18 @@
 def main():
-    pass
+    a,b = map(int, input().split())
+    lst = []
+    for i in range(3):
+        tmp = []
+        for j in range(3):
+            tmp.append(i*3+j+1)
+        lst.append(set(tmp))
+    
+    cur = set([a,b])
+    flg = False
+    for tmp in lst:
+        if cur <= tmp:
+            flg = True
+    print("Yes" if flg else "No")
 
 if __name__ == '__main__':
     main()
