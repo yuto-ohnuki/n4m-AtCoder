1e91318ea011d3aa6c12336270b36f84278c065d
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-28 12:23:14 +0900

    add abc309/c

--- ABC/abc309/c/main.py
+++ ABC/abc309/c/main.py
@@ -1,5 +1,17 @@
 def main():
-    pass
+    n,k = map(int, input().split())
+    lst = [list(map(int, input().split())) for _ in range(n)]
+    lst = sorted(lst, key=lambda x:x[0])
+    tot = sum([lst[i][1] for i in range(n)])
+
+    if tot <= k:
+        print(1)
+    else:
+        for a,b in lst:
+            tot -= b
+            if tot <= k:
+                break
+        print(a+1)
 
 if __name__ == '__main__':
     main()
