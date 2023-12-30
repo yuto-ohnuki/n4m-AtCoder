4c27b33b5edccf1508340f5ffcc14f057d5a80bc
yuto_ohnuki <yuto.personal92@gmail.com>
2023-12-30 14:28:40 +0900

    add abc301/b

--- ABC/abc301/b/main.py
+++ ABC/abc301/b/main.py
@@ -1,5 +1,22 @@
 def main():
-    pass
+    n = int(input())
+    a = list(map(int, input().split()))
+    ans = []
+
+    for i in range(n):
+        if i==0:
+            ans.append(a[i])
+        else:
+            cur = a[i]
+            pre = a[i-1]
+
+            if cur < pre:
+                for x in range(pre-1, cur-1, -1):
+                    ans.append(x)
+            elif cur > pre:
+                for x in range(pre+1, cur+1):
+                    ans.append(x)
+    print(*ans)
 
 if __name__ == '__main__':
     main()
