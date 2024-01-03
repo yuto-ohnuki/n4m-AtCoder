f029d3e243cf76ba30cdec1c76ebb66d0ec44099
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-03 22:27:24 +0900

    update abc319/b

--- ABC/abc319/b/main.py
+++ ABC/abc319/b/main.py
@@ -4,9 +4,9 @@ def main():
     for i in range(n+1):
         s = float('inf')
         for j in range(1, 10):
-            if n%j==0:
+            if n%j == 0:
                 tmp = n//j
-                if  i%tmp==0:
+                if i%tmp == 0:
                     s = min(s, j)
         if s != float('inf'):
             ans.append(str(s))
