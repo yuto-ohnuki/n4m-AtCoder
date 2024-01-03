8f6d994cc1a6b89d330c52b627e8592aa434521d
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-03 22:26:29 +0900

    add abc319/b

---
+++ ABC/abc319/b/main.py
@@ -0,0 +1,18 @@
+def main():
+    n = int(input())
+    ans = []
+    for i in range(n+1):
+        s = float('inf')
+        for j in range(1, 10):
+            if n%j==0:
+                tmp = n//j
+                if  i%tmp==0:
+                    s = min(s, j)
+        if s != float('inf'):
+            ans.append(str(s))
+        else:
+            ans.append('-')
+    print(''.join(ans))
+
+if __name__ == '__main__':
+    main()
