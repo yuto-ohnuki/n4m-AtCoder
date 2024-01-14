59126b0840c7a613c46f9d15629dc6e6c247588c
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-14 10:15:59 +0000

    add abc320/b

--- ABC/abc320/b/main.py
+++ ABC/abc320/b/main.py
@@ -1,5 +1,12 @@
 def main():
-    pass
+    s = input()
+    ans = -1
+    for i in range(len(s)+1):
+        for j in range(i+1, len(s)+1):
+            tmp = s[i:j]
+            if tmp == tmp[::-1]:
+                ans = max(ans, len(tmp))
+    print(ans)
 
 if __name__ == '__main__':
     main()
