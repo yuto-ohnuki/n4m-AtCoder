01ecb39081c94cb2951eeda3e42639b2e016ca60
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-14 10:25:09 +0000

    add abc321/b

---
+++ ABC/abc321/b/main.py
@@ -0,0 +1,15 @@
+def main():
+    n,x = map(int, input().split())
+    a = list(map(int, input().split()))
+    tot = sum(a)
+    ans = float('inf')
+    for nx in range(101):
+        tmp = a + [nx]
+        cur = tot + nx - max(tmp) - min(tmp)
+        if cur >= x:
+            ans = min(ans, nx)
+    print(ans if ans!=float('inf') else -1)
+
+
+if __name__ == '__main__':
+    main()
