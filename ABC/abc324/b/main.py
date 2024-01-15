f0438b04f3a13581239e6ccc529258ecd9a3bdef
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-15 06:19:46 +0000

    add abc324/b

--- ABC/abc324/b/main.py
+++ ABC/abc324/b/main.py
@@ -1,5 +1,11 @@
 def main():
-    pass
+    n = int(input())
+    flg = False
+    for x in range(70):
+        for y in range(40):
+            if 2**x * 3**y == n:
+                flg = True
+    print("Yes" if flg else "No")
 
 if __name__ == '__main__':
     main()
