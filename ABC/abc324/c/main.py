39fcd3a0334cd03abc5e0532b8da72c5d0a7121e
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-16 07:05:49 +0000

    add abc324/c

--- ABC/abc324/c/main.py
+++ ABC/abc324/c/main.py
@@ -1,5 +1,38 @@
+
+def func(x,y):
+    
+    # x: longer than y
+    if len(x) < len(y):
+        x,y = y,x
+    
+    rx = x[::-1]
+    ry = y[::-1]
+
+    if len(x) == len(y):
+        dif = 0
+        for i in range(len(x)):
+            if x[i]!=y[i]:
+                dif += 1
+        return dif <= 1
+    
+    else:
+        if len(x) - len(y) > 1:
+            return False
+        
+        else:
+            pass
+            # ここを修正する
+
 def main():
-    pass
+    n,t = input().split()
+    s = [input() for _ in range(int(n))]
+    ans = []
+    for i in range(int(n)):
+        if func(t, s[i]):
+            ans.append(i+1)
+
+    print(len(ans))
+    print(*ans)
 
 if __name__ == '__main__':
     main()
