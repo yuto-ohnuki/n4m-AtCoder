31d1812ec152368c4bb4861422871716d4acc7e1
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-16 07:22:24 +0000

    add abc325/b

--- ABC/abc325/b/main.py
+++ ABC/abc325/b/main.py
@@ -1,5 +1,18 @@
+from collections import defaultdict
 def main():
-    pass
+    n = int(input())
+    lst = [list(map(int, input().split())) for _ in range(n)]
+
+    dic = defaultdict(int)
+    for w,x in lst:
+        for i in range(x+9, x+18):
+            if i > 24:
+                i -= 24
+            dic[i] += w
+        
+    print(max(dic.values()))
+
+
 
 if __name__ == '__main__':
     main()
