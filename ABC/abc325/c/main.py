ff447fb14d93d2ac2ddcddd453c7e77aac075657
yuto_ohnuki <yuto.personal92@gmail.com>
2024-01-16 07:31:28 +0000

    add abc325/c

--- ABC/abc325/c/main.py
+++ ABC/abc325/c/main.py
@@ -1,5 +1,35 @@
+from collections import deque
 def main():
-    pass
+    h,w = map(int, input().split())
+    s = [list(input()) for _ in range(h)]
+
+    used = [[False]*w for _ in range(h)]
+    cnt = 0
+
+    for i in range(h):
+        for j in range(w):
+            if s[i][j] == '.':
+                used[i][j] = True
+            else:
+                if used[i][j]:
+                    continue
+                que = deque([(i,j)])
+                cnt += 1
+
+                while que:
+                    y,x = que.popleft()
+                    used[y][x] = True
+                    for di in range(-1, 2):
+                        for dj in range(-1, 2):
+                            ni, nj = y+di, x+dj
+                            if 0<=ni<h and 0<=nj<w:
+                                if s[ni][nj]=='#':
+                                    if not used[ni][nj]:
+                                        que.append((ni, nj))
+    
+    print(cnt)
+
+
 
 if __name__ == '__main__':
     main()
